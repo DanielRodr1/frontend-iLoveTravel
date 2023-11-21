e0774d567aaa051d184b10c037cd760247adfdf4
21MarcoBM <bmmarco056@gmail.com>
2023-11-21 06:05:53 -0500

    Login - Interface Hecha

--- src/app/public/sites/sites.module.ts
+++ src/app/public/sites/sites.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {SitesRoutingModule} from "./sites-routing.module";
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-
-  
-    HomeComponent
+    HomeComponent,
+          LoginComponent
   ],
   imports: [
     CommonModule,
