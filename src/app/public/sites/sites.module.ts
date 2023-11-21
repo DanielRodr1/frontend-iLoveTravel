180c001bc3d9fb7d8ad0a67e21409ff62692db32
21MarcoBM <bmmarco056@gmail.com>
2023-11-21 06:31:06 -0500

    Register - Interface Hecha

--- src/app/public/sites/sites.module.ts
+++ src/app/public/sites/sites.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {SitesRoutingModule} from "./sites-routing.module";
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
 
   
-    HomeComponent
+    HomeComponent,
+          RegisterComponent
   ],
   imports: [
     CommonModule,
