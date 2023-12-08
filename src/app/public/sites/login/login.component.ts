c6e2792855b191dcb004c3493c2ef482f93a35d1
Damian <damjeronzalez23@gmail.com>
2023-12-07 21:02:04 -0500

    add auth with api

--- src/app/public/sites/login/login.component.ts
+++ src/app/public/sites/login/login.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  constructor(private httpClient: HttpClient, private router: Router) {}
 
+  onLoginClick(): void {
+    const email = (document.getElementById('email') as HTMLInputElement)?.value;
+    const password = (document.getElementById('password') as HTMLInputElement)
+      ?.value;
+
+    const loginRequest = { email, password };
+
+    this.httpClient
+      .post('http://localhost:8080/api/v1/users/login', loginRequest)
+      .subscribe(
+        (response: any) => {
+          // Aquí puedes manejar la respuesta de la API, por ejemplo, redirigir al usuario a la página de inicio
+          console.log(response);
+          this.router.navigate(['../homeLogueado']);
+        },
+        (error) => {
+          // Aquí puedes manejar el error, por ejemplo, mostrar un mensaje de error al usuario
+          console.error(error);
+        }
+      );
+  }
 }
--- src/app/public/sites/register/register.component.ts
+++ src/app/public/sites/register/register.component.ts
@@ -1,10 +1,53 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  constructor(private httpClient: HttpClient, private router: Router) {}
 
+  onRegisterClick(): void {
+    const firstName = (document.getElementById('name') as HTMLInputElement)
+      .value;
+    const lastName = (document.getElementById('lastName') as HTMLInputElement)
+      .value;
+    const email = (document.getElementById('email') as HTMLInputElement).value;
+    const password = (document.getElementById('password') as HTMLInputElement)
+      .value;
+    const repeatPassword = (
+      document.getElementById('repeatPassword') as HTMLInputElement
+    ).value;
+    const nationality = (
+      document.getElementById('countries') as HTMLSelectElement
+    ).value;
+    const birthdate = (document.getElementById('birthDate') as HTMLInputElement)
+      .value;
+
+    // Validar aquí si las contraseñas coinciden u otras validaciones necesarias
+
+    const user = {
+      firstName,
+      lastName,
+      email,
+      password,
+      repeatPassword,
+      nationality,
+      birthdate,
+    };
+
+    this.httpClient
+      .post('http://localhost:8080/api/v1/users/register', user)
+      .subscribe(
+        (response: any) => {
+          this.router.navigate(['/login']);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+  }
 }
