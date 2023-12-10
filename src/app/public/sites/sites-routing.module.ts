192a2af197460bcc0b989e9871e5fd771741d2b0
Damian <damjeronzalez23@gmail.com>
2023-12-10 00:26:54 -0500

    add create article functionality

--- src/app/public/sites/sites-routing.module.ts
+++ src/app/public/sites/sites-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomelogueadoComponent } from 'src/app/intranet/homelogueado/homelogueado.component';
+import { CreateComponent } from 'src/app/intranet/create/create.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'homeLogueado', component: HomelogueadoComponent },
+  { path: 'createArticle', component: CreateComponent },
 ];
 
 @NgModule({
