4424a21c7f644995ecbe02893aa372097c460eee
Damian <damjeronzalez23@gmail.com>
2023-12-12 02:26:42 -0500

    add get all places in the page of intranet lugares

--- src/app/intranet/lugares/lugares.component.ts
+++ src/app/intranet/lugares/lugares.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lugares',
   templateUrl: './lugares.component.html',
-  styleUrls: ['./lugares.component.scss']
+  styleUrls: ['./lugares.component.scss'],
 })
-export class LugaresComponent {
+export class LugaresComponent implements OnInit {
+  private apiUrl = 'http://localhost:8080/api/v1/article/viewPlaces';
+  responseData: any;
+  displayedData: any;
+  logueado: boolean = false;
 
+  constructor(private httpClient: HttpClient, private router: Router) {}
+
+  ngOnInit(): void {
+    this.verificarTokenEnLocalStorage();
+    if (!this.logueado) {
+      this.router.navigate(['/']);
+    } else {
+      this.getDataFromApi();
+    }
+  }
+  verificarTokenEnLocalStorage(): void {
+    const token = localStorage.getItem('token');
+    this.logueado = Boolean(token) && token != '';
+  }
+
+  getDataFromApi(): void {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      const headers = new HttpHeaders({
+        Authorization: `Bearer ${token.trim()}`,
+        'Content-Type': 'application/json', // Puedes agregar otros encabezados según sea necesario
+      });
+
+      this.httpClient.get<any[]>(this.apiUrl, { headers }).subscribe(
+        (response) => {
+          this.responseData = response;
+          console.log(this.responseData);
+          this.displayedData = this.responseData;
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    } else {
+      console.error('No se encontró el token en localStorage.');
+    }
+  }
 }
