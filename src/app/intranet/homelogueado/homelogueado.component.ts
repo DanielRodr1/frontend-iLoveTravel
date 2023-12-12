65081e84d38faecfdea91938a7855dcdeb677c93
Damian <damjeronzalez23@gmail.com>
2023-12-12 00:52:30 -0500

    resolve errros and add redirect if not logued

--- src/app/intranet/homelogueado/homelogueado.component.ts
+++ src/app/intranet/homelogueado/homelogueado.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-homelogueado',
@@ -10,11 +11,21 @@ export class HomelogueadoComponent implements OnInit {
   private apiUrl = 'http://localhost:8080/api/v1/article/viewPlaces';
   responseData: any;
   displayedData: any;
+  logueado: boolean = false;
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient, private router: Router) {}
 
   ngOnInit(): void {
-    this.getDataFromApi();
+    this.verificarTokenEnLocalStorage();
+    if (!this.logueado) {
+      this.router.navigate(['/']);
+    } else {
+      this.getDataFromApi();
+    }
+  }
+  verificarTokenEnLocalStorage(): void {
+    const token = localStorage.getItem('token');
+    this.logueado = Boolean(token) && token != '';
   }
 
   getDataFromApi(): void {
@@ -40,5 +51,12 @@ export class HomelogueadoComponent implements OnInit {
     } else {
       console.error('No se encontró el token en localStorage.');
     }
+  }
+  cerrarSesion(): void {
+    // Elimina el token del localStorage
+    localStorage.removeItem('token');
+    // Actualiza la variable logueado
+    this.logueado = false;
+    // Puedes realizar otras acciones, como redirigir a la página de inicio de sesión, etc.
   }
 }
